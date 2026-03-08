15826367892d2bf83abaeb64dae3e9456cab723b
pn <bombaybrew.studio@gmail.com>
2026-03-08 12:44:54 +0530

    improve quality of preview

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const CONFIG = {
     targetUrl: process.env.TARGET_URL || 'http://localhost:7800',
     testerPort: parseInt(process.env.TESTER_PORT || '7801'),
     viewport: { width: 1280, height: 800 },
-    screenshotQuality: 15,
+    screenshotQuality: 40,
     heartbeatInterval: 1000,
     visualPause: 2000,
     maxPollingAttempts: 20,
